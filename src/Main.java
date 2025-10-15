c3b8c290b8387252a1d48b3e68b0829241cc42b8
1 <danilsnyiper@gmail.com>
2025-10-15 22:28:07 +0800

    сделана домашка

--- src/Main.java
+++ src/Main.java
@@ -2,101 +2,56 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-// задание 1
-        char clientOS = '1'; // 0 — iOS, 1 — Android
+        System.out.println("Задание 1");
+   for (int i = 1; i <= 10; i++){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Заднаие 2");
+   for (int i = 10; i >= 1; i--){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Задание 3");
+   for (int i = 0; i <= 17; i = i + 2){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Задание 4");
+   for (int i = 10; i >= (-10); i--){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Задание 5");
+   for (int i = 1904; i <= 2096; i = i + 4){
+       System.out.println(i + " год является високосным");
+   }
+        System.out.println("Задание 6");
+   for (int i = 7; i <= 98; i = i + 7){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Задание 7");
+   for (int i = 1; i <= 512; i = i * 2){
+       System.out.println("Число " + i);
+   }
+        System.out.println("Задание 8");
+   int mothSave = 29000;
+   int totalSave = 0;
+   for (int moth = 1; moth <= 12; moth++){
+       totalSave += mothSave;
+       System.out.println("Месяц " + moth + ", сумма накоплений равна " + totalSave + " рублей");
+   }
+        System.out.println("Задание 9");
+   int mothSave1 = 29000;
+   double totalSave1 = 0;
+   double mothRate = 0.12 / 12;
+   for (int moth = 1; moth <= 12; moth++) {
+       totalSave1 += mothSave1;
+       double mothproc = totalSave1 * mothRate;
+       totalSave1 += mothproc;
+       System.out.println("Месяц " + moth + ", сумма накоплений равна " + totalSave1 + " рублей");
+   }
+        System.out.println("Задание 10");
+   for (int i = 1; i <= 10; i++){
+       int result = 2 * i;
+       System.out.println("2*" + i + "=" + result);
+   }
 
-        if (clientOS == '0') {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == '1') {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        // задание 2
-        char clientOS1 = '0'; // 0 — iOS, 1 — Android
-        int clientDeviceYear = 2015;
-
-        boolean isOldDevice = clientDeviceYear <= 2015;
-        boolean isNewDevice = clientDeviceYear > 2015;
-        boolean IOS = clientOS1 == '0';
-        boolean Android = clientOS1 == '1';
-
-        if (IOS && isOldDevice) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (IOS && isNewDevice) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (Android && isOldDevice) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (Android && isNewDevice) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        // задание 3
-        int year = 2021;
-        int veak = 100;
-        int veak4 = 400;
-        int years4 = 4;
-        if ((year % years4 == 0 && year % veak != 0 ) || (year % veak4 == 0)){
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
-        // задание 4
-        int deliveryDistance = 95;
-        int deliveryDays = 1;
-        int deliveryDaysTwo = 2;
-        int deliveryDaysThree = 3;
-        if (deliveryDistance >= 100){
-            System.out.println("Доставки нет");
-        }
-        else if (deliveryDistance <= 20 ) {
-            System.out.println("Потребуется дней: " + deliveryDays);
-        }
-        else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + deliveryDaysTwo);
-        }
-        else {
-            System.out.println("Потребуется дней: " + deliveryDaysThree);
-        }
-        // задание 5
-        int monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-                System.out.println("Январь, принадлежит к сезону зима");
-                break;
-            case 2:
-                System.out.println("Ферваль, принадлежит к сезону зима");
-                break;
-            case 3:
-                System.out.println("Март, принадлежит к сезону весна");
-                break;
-            case 4:
-                System.out.println("Апрель, принадлежит к сезону весна");
-                break;
-            case 5:
-                System.out.println("Май, принадлежит к сезону весна");
-                break;
-            case 6:
-                System.out.println("Июнь, принадлежит к сезону лето");
-                break;
-            case 7:
-                System.out.println("Июль, принадлежит к сезону лето");
-                break;
-            case 8:
-                System.out.println("Август, принадлежит к сезону лето");
-                break;
-            case 9:
-                System.out.println("Сентябрь, принадлежит к сезону осень");
-                break;
-            case 10:
-                System.out.println("Октябрь, принадлежит к сезону осень");
-                break;
-            case 11:
-                System.out.println("Ноябрь, принадлежит к сезону осень");
-                break;
-            case 12:
-                System.out.println("Декабрь, принадлежит к сезону зима");
-                break;
-            default:
-                System.out.println("Номер месяца больше 12");
-
-        }
     }
 }
